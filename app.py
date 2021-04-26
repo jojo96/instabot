7f3ba0875851cb5648b3a46dc4000be732b60ae0
Ujjayanta <aprajit09041996@gmail.com>
2021-04-26 13:27:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template, request
 #from chatterbot import ChatBot
 #from chatterbot.trainers import ChatterBotCorpusTrainer
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 #english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter") #original line
 #english_bot = ChatBot("Chatterbot",read_only = False,logic_adapters = ['chatterbot.logic.MathematicalEvaluation','chatterbot.logic.BestMatch'],preprocessors=['chatterbot.preprocessors.clean_whitespace','chatterbot.preprocessors.convert_to_ascii','chatterbot.preprocessors.unescape_html'], storage_adapter="chatterbot.storage.SQLStorageAdapter") #original line
 #trainer = ChatterBotCorpusTrainer(english_bot)
 #trainer.train("chatterbot.corpus.english")
 
-@app.route("/")
+@application.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/get")
+@application.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
     msg = userText.lower()
@@ -95,4 +95,4 @@ def get_bot_response():
 
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
